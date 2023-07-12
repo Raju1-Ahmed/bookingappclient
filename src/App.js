7449520e183035003246bd68454a76cbd0ade04e
Raju1-Ahmed <robiussaniraju@gmail.com>
2023-07-12 16:25:38 +0600

    filtaring  done

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/hotels" element={<List/>}/>
-        <Route path="/hotels/:id" element={<Hotel/>}/>
+        <Route path="/hotel/:id" element={<Hotel/>}/>
+        {/* <Route path="/hotels/:id" element={<Hotel/>}/> */}
       </Routes>
     </BrowserRouter>
   );
